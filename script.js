3c540f244d636ddffdb55904c79d65ba71e56913
Piyush <piyushpratapsingh67@gmail.com>
2024-07-23 15:36:40 +0530

    Portfolio_Update: 1.5 - Changing the minor details and adding animations

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
 const sideMenu = document.getElementById("sideMenu");
 
+const navBar = document.querySelector('nav');
+const navLinks = document.querySelector('nav ul');
+
 function openMenu() {
   sideMenu.style.transform = 'translateX(-16rem)';
 }
 function closeMenu() {
   sideMenu.style.transform = 'translateX(16rem)';
+}
+
+window.addEventListener('scroll', () => {
+  if (scrollY > 50) {
+    navBar.classList.add('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+    navBar.style.transition = 'all 0.5s ease';
+
+    navLinks.classList.remove('bg-white', 'shadow-sm', 'bg-opacity-50');
+    navLinks.style.transition = 'all 0.5s ease';
+  } else {
+    navBar.classList.remove('bg-white', 'bg-opacity-50', 'backdrop-blur-lg', 'shadow-sm');
+    navBar.style.transition = 'all 0.5s ease';
+
+    navLinks.classList.add('bg-white', 'shadow-sm', 'bg-opacity-50');
+    navLinks.style.transition = 'all 0.5s ease';
+  }
+})
+
+
+//Tailwind Configuration
+
+tailwind.config = {
+  theme: {
+    gridTemplateColumns: {
+      'auto': 'repeat(auto-fit, minmax(200px, 1fr))'
+    },
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+      Ovo: ["Ovo", "serif"]
+    },
+    animation: {
+      'spin_slow': 'spin 6s linear infinite',
+    },
+  }
 }
